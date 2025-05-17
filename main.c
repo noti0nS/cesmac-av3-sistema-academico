5c58fb40a50d7d72790cffe579d9a9f4f7a8f1ee
noti0nS <joselucas0303@gmail.com>
2025-05-17 18:38:43 -0300

    feat: implement auth and overall program's structure

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <locale.h>
 
+#define USERNAME "admin"
+#define PASSWORD "admin"
 
 struct Aluno
 {
@@ -21,43 +24,100 @@ struct Disciplina
   struct AlunoDisciplinaNota aluno_notas[500];
 };
 
-void exibir_banner() {
-  printf("====================================\n");
-  printf("         BEM VINDO AO PROJETO       \n");
-  printf("====================================\n");
+void cadastrar_disciplina(struct Disciplina *disciplina)
+{
+  printf("Digite o título da disciplina: ");
+  scanf("%s", disciplina->titulo);
+}
+
+void exibir_banner()
+{
+printf("   ▄████████  ▄████████    ▄████████ ████████▄  ▀████    ▐████▀ \n");
+printf("  ███    ███ ███    ███   ███    ███ ███   ▀███   ███▌   ████▀  \n");
+printf("  ███    ███ ███    █▀    ███    ███ ███    ███    ███  ▐███    \n");
+printf("  ███    ███ ███          ███    ███ ███    ███    ▀███▄███▀    \n");
+printf("▀███████████ ███        ▀███████████ ███    ███    ████▀██▄     \n");
+printf("  ███    ███ ███    █▄    ███    ███ ███    ███   ▐███  ▀███    \n");
+printf("  ███    ███ ███    ███   ███    ███ ███   ▄███  ▄███     ███▄  \n");
+printf("  ███    █▀  ████████▀    ███    █▀  ████████▀  ████       ███▄ \n");
+printf("                                                                \n");
+
+}
+
+void limpar_tela()
+{
+  system("clear || cls");
+}
+
+void autenticar_usuario()
+{
+  printf("Digite suas credenciais para se autenticar no sistema.\n");
+
+  char usuario[20];
+  char senha[20];
+
+  while (1)
+  {
+    printf("Usuário: ");
+    scanf("%s", &usuario);
+    printf("Senha: ");
+    scanf("%s", &senha);
+
+    if (strcmp(usuario, USERNAME) == 0 && strcmp(senha, PASSWORD) == 0)
+    {
+      break;
+    }
+    printf("Usuário ou senha inválidos. Tente novamente.\n");
+  }
 }
 
 int main()
 {
+  setlocale(LC_ALL, "Portuguese");
 
-  setlocale(LC_ALL,"");
+  autenticar_usuario();
+  limpar_tela();
+  exibir_banner();
+
   int option = 0;
   do
   {
-    exibir_banner();
-    printf("Escolha uma opção:\n");
-    printf("1. Opção 1\n");
-    printf("2. Opção 2\n");
-    printf("3. Sair\n");
-    printf("Digite sua opção: ");
+    printf("1 - Exibir Disciplinas\n");
+    printf("2 - Cadastrar Disciplinas\n");
+    printf("3 - Exibir Alunos Matriculados\n");
+    printf("4 - Matricular Aluno\n");
+    printf("5 - Atribuir Nota\n");
+    printf("6 - Sair\n");
+    printf("> ");
+
     scanf("%d", &option);
 
     switch (option)
     {
-      case 1:
-        printf("Você escolheu a Opção 1.\n");
-        break;
-      case 2:
-        printf("Você escolheu a Opção 2.\n");
-        break;
-      case 3:
-        printf("Saindo...\n");
-        break;
-      default:
-        printf("Opção inválida! Tente novamente.\n");
-        break;
+    case 1:
+      printf("TODO\n");
+      break;
+    case 2:
+      printf("TODO\n");
+      break;
+    case 3:
+      printf("TODO\n");
+      break;
+    case 4:
+      printf("TODO\n");
+      break;
+    case 5:
+      printf("TODO\n");
+      break;
+    case 6:
+      limpar_tela();
+      printf("Obrigado por usar o nosso sistema! Até a próxima :-)\n");
+      break;
+    default:
+      printf("Opção inválida! Tente novamente.\n");
+      break;
     }
-  } while (option != 3);
-  
+  } while (option != 6);
+
   return 0;
 }
